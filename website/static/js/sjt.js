3b2b3b02b70614ebb14f1399a9a7119a04bf8cdc
Mitchell Fukumoto <mitchsf777@gmail.com>
2014-12-21 15:40:35 -0800

    stabilize menu

--- website/static/js/sjt.js
+++ website/static/js/sjt.js
@@ -7,10 +7,12 @@ menuOpener.addEventListener('click', function() {
   menuContent.classList.toggle('menu-open');
   mainContent.classList.toggle('menu-open');
   nav.classList.toggle('menu-open');
+  document.body.classList.toggle('menu-open');
 });
 
 mainContent.addEventListener('click', function() {
   menuContent.classList.remove('menu-open');
   mainContent.classList.remove('menu-open');
   nav.classList.remove('menu-open');
+  document.body.classList.remove('menu-open');
 });
