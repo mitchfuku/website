7df9d5a1cb7d8f97d55714211d6d26754e8d2b60
Mitchell Fukumoto <mitchsf777@gmail.com>
2016-01-03 15:17:17 -1000

    adding data json blob

---
+++ website/static/js/sw/SWData.js
@@ -0,0 +1,520 @@
+SWData = [
+  {
+    name: 'A New Threat',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '01A', '02A', '04A', '05A', '07A', '10A', '12A', '14A', '14A', '17A', '22B', '25B', '28B', '31B', '32B', '33B', '35B', '36B', '36B', '38B', '38B', '38B'
+    ],
+  },
+  {
+    name: 'A Simple Task',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '02B', '07B', '17B', '18B', '19A', '23A', '24A', '28A', '32A', '36A', '36A', '38A'
+    ],
+  },
+  {
+    name: 'Aftermath',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '07A', '10A', '18A', '22B', '25B', '32B', '35B', '36B', '36B', '38B'
+    ],
+  },
+  {
+    name: 'Armed and Operational',
+    wave: {
+      name: 'Wave 1',
+      number: 1,
+    },
+    tiles: [
+      '01B', '02B', '10B', '14B', '14B', '15B', '17B', '18B', '18B', '19B', '34B', '36B', '36B', '39B'
+    ],
+  },
+  {
+    name: 'Back-Room Bargains',
+    wave: {
+      name: 'Wave 3',
+      number: 3,
+    },
+    tiles: [
+      '20B', '21B', '22B', '23B', '24B', '25B', '27B', '28B', '32B', '32B', '35B', '36B', '36B', '36B'
+    ],
+  },
+  {
+    name: 'Binary Revolution',
+    wave: {
+      name: 'Wave 1',
+      number: 1,
+    },
+    tiles: [
+      '22A', '23A', '24A', '27A', '28A', '30A', '32A', '32A', '25A', '25A', '36A'
+    ],
+  },
+  {
+    name: 'Brace for Impact',
+    wave: {
+      name: 'Wave 1',
+      number: 1,
+    },
+    tiles: [
+      '02A', '03A', '06A', '14A', '15A', '18A', '18A', '18A', '21A', '32A', '34A', '35A', '36A', '36A', '39A'
+    ],
+  },
+  {
+    name: 'Breaking Point',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '18B', '20B', '21B', '24B', '25B', '28B', '31B', '39B'
+    ],
+  },
+  {
+    name: 'Brushfire',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '02A', '04A', '05A', '06A', '07A', '10A', '11A', '12A', '13A', '14A', '14A', '16A', '17A'
+    ],
+  },
+  {
+    name: 'Brute Force',
+    wave: {
+      name: 'Wave 3',
+      number: 3,
+    },
+    tiles: [
+      '03A', '05A', '13A', '18A', '18A', '18A', '21A', '28A', '36A', '36A', '39A'
+    ],
+  },
+  {
+    name: 'Bunker Buster',
+    wave: {
+      name: 'Wave 3',
+      number: 3,
+    },
+    tiles: [
+      '02B', '07B', '10B', '13B', '14B', '14B', '18B', '20B', '27B', '31B', '32B', '32B', '39B', '39B'
+    ],
+  },
+  {
+    name: 'Canyon Run',
+    wave: {
+      name: 'Wave 2',
+      number: 2,
+      details: 'Twin Shadows',
+    },
+    tiles: [
+      'TS04B', 'TS05B', 'TS06B', '07B', '10B', '14B', '15B', '18B', '18B', '18B', '23A', '31A', '32A', '36A', '36A', '38A'
+    ],
+  },
+  {
+    name: 'Captured',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '19A', '21A', '22A', '23A', '24A', '25A', '27A', '28A', '30A', '32A', '32A', '33A', '34A', '36A', '36B', '36B', '37B'
+    ],
+  },
+  {
+    name: 'Celebration',
+    wave: {
+      name: 'Wave 1',
+      number: 1,
+    },
+    tiles: [
+      '03A', '04A', '06A', '07A', '14A', '17A', '17A', '18A', '18A', '18A', '22A', '36A', '38A'
+    ],
+  },
+  {
+    name: 'Chain of Command',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '19A', '21A', '22A', '23A', '24A', '27A', '28A', '36A', '36A', '36A'
+    ],
+  },
+  {
+    name: 'Dark Obsession',
+    wave: {
+      name: 'Core Game',
+      details: 'Included',
+      number: 0,
+    },
+    tiles: [
+      '19A', '20A', '24A', '27A', '28A', '35A', '35A', '36A', '36A', '36A', '36A'
+    ],
+  },
+  {
+    name: 'Desperate Hour',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '02B', '05B', '06B', '10B', '13B', '14B', '17B', '18B', '19A', '20A', '23A', '24B', '28A', '32B', '32B', '37B', '39B'
+    ],
+  },
+  {
+    name: 'Drawn In',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '19A', '20A', '21A', '23A', '24A', '25A', '26A', '28A', '31A', '32A', '36A', '36A'
+    ],
+  },
+  {
+    name: 'Fire in the Sky',
+    wave: {
+      name: 'Wave 2',
+      number: 2,
+      details: 'Twin Shadows',
+    },
+    tiles: [
+      'TS01A', 'TS02A', 'TS03A', 'TS04A', 'TS05A', 'TS07A', '19A', '21A', '32A', '32A', '34A', '36A', '36A', '36A'
+    ],
+  },
+  {
+    name: 'Fly Solo',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '04B', '06B', '11B', '12B', '13B', '14B', '16B', '17B', '18B', '18B', '21B', '22B', '24B', '32B', '34B', '35B', '36B', '39B', '39B'
+    ],
+  },
+  {
+    name: 'Forest Ambush',
+    wave: {
+      name: 'Wave 1',
+      number: 1,
+    },
+    tiles: [
+      '01A', '02A', '04A', '06A', '18A', '18A', '18A', '18A'
+    ],
+  },
+  {
+    name: 'Friends of Old',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '04A', '05A', '06A', '07A', '09A', '11A', '12A', '14A', '17A', '17A', '18A'
+    ],
+  },
+  {
+    name: 'Generous Donations',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '01A', '02A', '04A', '10A', '18A', '22A', '23A', '24A', '32A', '39A', '39A'
+    ],
+  },
+  {
+    name: 'High Moon',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '03B', '05B', '06B', '07B', '10B', '11B', '18B', '18B', '21B', '36B', '36B', '39B'
+    ],
+  },
+  {
+    name: 'Homecoming',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '01B', '02B', '04B', '05B', '06B', '07B', '10B', '11B', '17B', '17B', '18B'
+    ],
+  },
+  {
+    name: 'Hunted Down',
+    wave: {
+      name: 'Wave 2',
+      number: 2,
+      details: 'Twin Shadows',
+    },
+    tiles: [
+      'TS01B', '01B', '02B', '18B', '18B', '20B', '36B', '36B', '39B'
+    ],
+  },
+  {
+    name: 'Imperial Entanglements',
+    wave: {
+      name: 'Wave 1',
+      number: 1,
+    },
+    tiles: [
+      '21A', '22A', '23A', '24A', '28A', '30A', '31A', '32A', '32A', '34A', '36A', '36A'
+    ],
+  },
+  {
+    name: 'Imperial Hospitality',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '20A', '21A', '22A', '23A', '24A', '25A', '27A', '28A', '29A', '33A', '35A', '35A', '36A'
+    ],
+  },
+  {
+    name: 'Impounded',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '01B', '02B', '05B', '10B', '14B', '18B', '22A', '23A', '25A', '28A', '31A', '35A', '36A', '36A', '38A', '38A'
+    ],
+  },
+  {
+    name: 'Incoming',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '03A', '05A', '10A', '12A', '14A', '14A', '22B', '23B', '24B', '25B', '26B', '28B', '32B', '32B', '36B', '36B', '36B', '36B', '38B'
+    ],
+  },
+  {
+    name: 'Indebted',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '01A', '04A', '06A', '18A', '22A', '23A', '28A', '33A', '36A', '36A', '39A', '39A', '39A'
+    ],
+  },
+  {
+    name: 'Infection',
+    wave: {
+      name: 'Wave 1',
+      number: 1,
+    },
+    tiles: [
+      '01B', '18B', '18B', '21A', '23A', '24A', '27A', '32A', '36A', '36A', '36A', '38A'
+    ],
+  },
+  {
+    name: 'Infiltrated',
+    wave: {
+      name: 'Wave 2',
+      number: 2,
+      details: 'Twin Shadows',
+    },
+    tiles: [
+      'TS01A', 'TS02A', 'TS03A', 'TS04A', 'TS05A', 'TS07A', '19A', '21A', '32A', '32A', '34A', '36A', '36A', '36A'
+    ],
+  },
+  {
+    name: 'Last Stand',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '02B', '05B', '06B', '10B', '13B', '14B', '17B', '18B', '19A', '20A', '23A', '24B', '28A', '32B', '32B', '37B', '39B'
+    ],
+  },
+  {
+    name: 'Loose Cannon',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '02B', '11B', '12B', '13B', '14B', '14B', '17B', '17B', '17B', '19A', '29A', '32A', '36A', '36A', '38A', '38A'
+    ],
+  },
+  {
+    name: 'Luxury Cruise',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '20A', '21A', '23A', '24A', '25A', '28A', '29A', '31A', '32A', '32A', '35A', '35A', '35A'
+    ],
+  },
+  {
+    name: 'Means of Production',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '05A', '07A', '10A', '18A', '19A', '22A', '23A', '27A', '28A', '33A', '39A'
+    ],
+  },
+  {
+    name: 'Open to Interpretation',
+    wave: {
+      name: 'Wave 2',
+      number: 2,
+    },
+    tiles: [
+      'TS04B', 'TS06B', '11B', '13B', '14B', '14B', '18B', '18B', '22B', '23B', '27B', '28B', '29B', '36B', '36B', '36B', '39B'
+    ],
+  },
+  {
+    name: 'Past Life Enemies',
+    wave: {
+      name: 'Wave 2',
+      number: 2,
+      details: 'Twin Shadows',
+    },
+    tiles: [
+      'TS03A', 'TS06A', 'TS07B', '05B', '17B', '22B', '23B', '28B', '32B', '36B', '37B', '38A'
+    ],
+  },
+  {
+    name: 'Predator and Prey',
+    wave: {
+      name: 'Wave 2',
+      number: 2,
+    },
+    tiles: [
+      '01A', '02A', '05A', '06A', '09A', '12A', '13A', '14A', '14A', '17A', '18A', '18A', '18A', '18A'
+    ],
+  },
+  {
+    name: 'Shady Dealings',
+    wave: {
+      name: 'Wave 2',
+      number: 2,
+      details: 'Twin Shadows',
+    },
+    tiles: [
+      'TS02B', 'TS03B', 'TS07B', '06B', '21B', '23B', '28B', '29B', '31B', '32B', '32B', '35B', '35B', '35B', '36B', '36B', '36B', '36B', '39B', '39B'
+    ],
+  },
+  {
+    name: 'Sorry About the Mess',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+       '01B', '3B', '10B', '14B', '18B', '18B', '21B', '32B', '36B', '39B', '39B'
+    ],
+  },
+  {
+    name: 'Strength of Command',
+    wave: {
+      name: 'Wave 2',
+      number: 2,
+    },
+    tiles: [
+      'TS01A', '21A', '26A', '27A', '28A', '29A', '31A', '32A', '33A', '34A', '34A', '35A', '36A', '36A', '36A', '36A'
+    ],
+  },
+  {
+    name: 'Sympathy for the Rebellion',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+      details: 'Included',
+    },
+    tiles: [
+      '02B', '18B', '18B', '21B', '22B', '25B', '26B', '28B', '30B', '36B', '36B', '39B', '39B'
+    ],
+  },
+  {
+    name: 'Target of Opportunity',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '21A', '22A', '23A', '26A', '28A', '30A', '32A', '33A', '36A', '36A', '36A', '36A'
+    ],
+  },
+  {
+    name: 'Temptation',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '02A', '09A', '10A', '13A', '14A', '18A', '18A', '27B', '28B', '32B', '32B', '36B', '38B', '38B'
+    ],
+  },
+  {
+    name: 'The Source',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '19B', '20B', '21A', '22A', '23B', '24B', '28A', '29A', '30B', '31A', '35A', '35B', '36A', '36A', '36B', '36B', '37B'
+    ],
+  },
+  {
+    name: 'The Spice Job',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '19B', '20B', '23B', '24B', '28B', '36B'
+    ],
+  },
+  {
+    name: 'Under Siege',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '01A', '02A', '07A', '12A', '18A', '20A', '22A', '23A', '26A', '29A', '30A', '31A', '32A', '36A', '36A', '39A', '39A'
+    ],
+  },
+  {
+    name: 'Viper\'s Den',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '03B', '09B', '11B', '14B', '14B', '17B', '18B', '22B', '24B', '29B', '36B', '39B', '39B'
+    ],
+  },
+  {
+    name: 'Wanted',
+    wave: {
+      name: 'Core Game',
+      number: 0,
+    },
+    tiles: [
+      '21B', '22B', '24B', '26B', '27B', '29B', '32B', '32B', '35B', '36B', '36B', '36B'
+    ],
+  },
+];
